39a71ecf1ed6ca130284a841c553c80c837fbb87
= <=>
2019-06-27 09:12:53 +0430

    reports updated

--- app/src/main/java/com/xamuor/cashco/pos/PosDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.xamuor.cashco.pos;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {Product.class, Customer.class}, version = 1)
-public abstract class PosDatabase extends RoomDatabase {
-    public abstract ProductDAO myDao();
-}
